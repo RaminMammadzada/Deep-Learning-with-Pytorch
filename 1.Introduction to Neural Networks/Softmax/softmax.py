281b0ba04f8cf8ae78f42399163d01e700b42362
Ramin <remki-design@hotmail.com>
2018-11-15 17:20:39 +0300

    Implement Softmanx Algorithm

---
+++ 1.Introduction to Neural Networks/Softmax/softmax.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+
+
+# calculate the sum of the exponential of each score
+def sumOfExp(L):
+    # input: L - the list of lineer function scores
+    # output: the sum of the exponentials of each scores
+
+    sum = 0
+    for score in L:
+        sum += np.exp(score)
+    return sum
+
+# calculates the probability of the score
+def probability(score,L):
+    # input: score - the lineer function score
+    #        L - the list of lineer function scores
+    # output: prob - the probability of the score
+    
+    prob = np.exp(score) / sumOfExp(L)
+    return prob
+
+# gives the probabilities of the scores as a list
+def softmax(L):
+    # input: L - list of lineer function scores
+    # output: probs - list of probabilites of the scores in the list L
+    
+    probs = []
+    sizeOfListOfScores = len(L)
+    for score in L:
+        probs.append(probability(score,L))
+
+    return probs
