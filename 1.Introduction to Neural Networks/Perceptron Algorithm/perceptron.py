05134803dba2e01dcdf346994b098ddf4a3b2f95
Ramin <remki-design@hotmail.com>
2018-11-24 08:41:12 +0300

    Add solution to the Perceptron Algorithm.

--- 1.Introduction to Neural Networks/Perceptron Algorithm/perceptron.py
+++ 1.Introduction to Neural Networks/Perceptron Algorithm/perceptron.py
@@ -18,6 +18,16 @@ def prediction(X, W, b):
 # and return W and b.
 def perceptronStep(X, y, W, b, learn_rate = 0.01):
     # Fill in code
+    for i in range(len(X)):
+        y_hat = prediction(X[i],W,b)
+        if y[i]-y_hat == 1:
+            W[0] += X[i][0]*learn_rate
+            W[1] += X[i][1]*learn_rate
+            b += learn_rate
+        elif y[i]-y_hat == -1:
+            W[0] -= X[i][0]*learn_rate
+            W[1] -= X[i][1]*learn_rate
+            b -= learn_rate
     return W, b
     
 # This function runs the perceptron algorithm repeatedly on the dataset,
